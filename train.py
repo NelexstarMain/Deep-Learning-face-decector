be66ab3f1f1976fdfe924302f3a9b33231e50f3b
NelexstarMain <kbartlewski5@gmail.com>
2024-10-27 19:34:56 +0100

    Update train.py
    adding augmentation and adding a test data set from the main data set

--- train.py
+++ train.py
@@ -1,36 +1,60 @@
 import pickle
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+
+
+
+
+
 
 x = pickle.load(open("X.pk1", "rb"))
 y = pickle.load(open("Y.pk1", "rb"))
 
-x = x/255
+
+x = x.astype('float32')  
+x /= 255.0  
+
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+datagen = ImageDataGenerator(
+    rotation_range=40,  
+    width_shift_range=0.2, 
+    height_shift_range=0.2,  
+    shear_range=0.2,  
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest' 
+)
+
 
 model = Sequential()
 
-model.add(Conv2D(64, (3, 3), activation = "relu"))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=x.shape[1:]))
 model.add(MaxPooling2D((2, 2)))
 
-model.add(Conv2D(64, (3, 3), activation = "relu"))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2, 2)))
 
 model.add(Flatten())
 
-model.add(Dense(128, input_shape = (100, 100, 3)))
 
-model.add(Dense(128, input_shape = (100, 100, 3)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(3, activation='softmax')) 
 
-model.add(Dense(256, input_shape = (100, 100, 3)))
 
-model.add(Dense(128, input_shape = (100, 100, 3)))
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
-model.add(Dense(128, input_shape = (100, 100, 3)))
 
-model.add(Dense(3, activation = "softmax"))
+model.fit(datagen.flow(x_train, y_train, batch_size=32),
+          epochs=10)
 
-model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics=["accuracy"])
+test_loss, test_acc = model.evaluate(x_val, y_val)
+print('Test accuracy:', test_acc)
 
-model.fit(x, y, epochs = 7, validation_split=0.001)
-
-model.save("model.h5")
+model.save('model.h5')
